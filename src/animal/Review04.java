e9de12d6c5b44c9488869bf6c95c8f22a30f09d6
staff0041 <hkosuteui@hotmail.co.jp>
2024-07-11 17:03:44 +0900

    Pass your hobby along with it when creating an instance.

--- src/animal/Review04.java
+++ src/animal/Review04.java
@@ -2,15 +2,9 @@ package animal;
 
 public class Review04 {
     public static void main(String[] args) {
-        Human tanaka = new Human("田中 太郎", 25);
-        tanaka.setHobby("電車");
-
-        Human suzuki = new Human("鈴木 次郎", 30);
-        suzuki.setHobby("野球");
-
-        Human sato = new Human("佐藤 花子", 20);
-        sato.setHobby("映画");
-
+        Human tanaka = new Human("田中 太郎", 25, "電車");
+        Human suzuki = new Human("鈴木 次郎", 30, "野球");
+        Human sato = new Human("佐藤 花子", 20, "映画");
 
         tanaka.say();
         tanaka.think();
@@ -24,4 +18,3 @@ public class Review04 {
     }
 
 }
-
